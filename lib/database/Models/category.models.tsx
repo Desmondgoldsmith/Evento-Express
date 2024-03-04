162bb21c3fdb2adb9d7f381db05a58933879aeda
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-03-04 22:09:14 +0000

    Adding Mongo and Mongoose

--- lib/database/Models/category.models.tsx
+++ lib/database/Models/category.models.tsx
@@ -1,8 +1,12 @@
 import { Schema } from "mongoose";
 
+export interface ICategory extends Document{
+ _id: string
+ name: string
+}
 
 const categorySchema = new Schema({
     name: {type: String, required: true, unique: true},
-    
+      
 
 })
