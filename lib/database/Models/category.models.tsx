b6ac7a9148b4bdcce50180ea122a51d5dcc2bd98
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-03-04 22:12:42 +0000

    Adding Mongo and Mongoose

--- lib/database/Models/category.models.tsx
+++ lib/database/Models/category.models.tsx
@@ -9,4 +9,6 @@ const categorySchema = new Schema({
     name: {type: String, required: true, unique: true},
 })
 
-const Category = models.Category || model('Category', CategorySchema)
+const Category = models.Category || model('Category', categorySchema)
+
+export default Category
